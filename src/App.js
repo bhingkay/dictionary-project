8161e21012165c431003bd829385ba6aa62d6d35
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-04-26 13:05:27 +0300

    added background image

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="App-header"></div>
+      </div>
+    </div>
+  );
+}
