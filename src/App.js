ae9f073e8c6cc37b7d488dccb842fc76984c5451
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-04-27 13:58:19 +0300

    updated css

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <div className="main">
-        <div className="App-header">
-          <Dictionary defaultKeyword="" />
+      <div className="container">
+        <div className="main">
+          <div className="App-header">
+            <Dictionary defaultKeyword="" />
+          </div>
         </div>
+        <footer>
+          <p>
+            Open Source code by: Bhingkay on {""}
+            <a
+              href="https://github.com/bhingkay/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <span className="vecteezy">
+              Vector Vectors by: {""}
+              <a href="https://www.vecteezy.com/free-vector/vector">Vecteezy</a>
+            </span>
+          </p>
+        </footer>
       </div>
-      <footer>
-        <p>
-          Open Source code by: Bhingkay on {""}
-          <a
-            href="https://github.com/bhingkay/dictionary-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-          <span className="vecteezy">
-            Cute Book Vectors by: {""}
-            <a href="https://www.vecteezy.com/free-vector/nature">Vecteezy</a>
-          </span>
-        </p>
-      </footer>
     </div>
   );
 }
